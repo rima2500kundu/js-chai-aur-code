be8e9f1b115c0491e3dc71de7f82c995d5065bb1
Rima Kundu <120553319+rima2500kundu@users.noreply.github.com>
2024-05-01 11:20:51 +0000

    modified 01_variables.js and added 01_datatypes.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,18 @@ const accountId = 144553;
 let accountEmail = "kundu.rima64@gmail.com";
 var accountPassword = "12345";
 accountCity = "Jaipur";
+let accountState;
 
-accountId = 2;
+// accountId = 2; // not allowed
+accountEmail = "puma@gmail.com";
+accountPassword = "Rp524";
+accountCity = "Bangaluru";
+
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
